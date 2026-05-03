3506eb0e62f8aab446869f8aeb3f30cb90aba3ee
Juddan <juddan2008@gmail.com>
2026-05-03 18:10:52 +0530

    fix: add sslmode=disable for PostgreSQL

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
 
+const dbUrl = process.env.DATABASE_URL!.includes('sslmode') 
+  ? process.env.DATABASE_URL! 
+  : process.env.DATABASE_URL! + '?sslmode=disable';
+
 export default defineConfig({
   schema: './src/database/schema/index.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: dbUrl,
   },
 });
